c50bf26833d06a21aca74237dcef913ef31a9e52
Alexis Astolfi <alexis.astolfi@gmail.com>
2018-02-21 04:29:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-var name = "Joe";
-var height = "74";
-
-if (name === "Susan") {
-  console.log("The name is correct");
-} else {
-  console.log("Expectd the name to be Susan");
-}
-
+var name = "Joe"
+var height = "74"
 
 
 // Don't worry about this
